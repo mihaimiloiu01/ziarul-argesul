5e7aa9a9e7e88029866f72a6242685ecfda8eb94
mihai.miloiu <mihaimiloiu20@gmail.com>
2025-10-01 15:04:46 +0300

    Updated production admob ids

--- lib/ads/ad_helper.dart
+++ lib/ads/ad_helper.dart
@@ -4,11 +4,11 @@ class AdHelper {
 
   static String get bannerAdUnitId {
     if (Platform.isAndroid) {
-      // Production ID: return 'ca-app-pub-4470739099972924~2355457446';
-      return 'ca-app-pub-3940256099942544/6300978111';
+      return 'ca-app-pub-5562747729301019/1894338733';
+      // Test unit ID: return 'ca-app-pub-3940256099942544/6300978111';
     } else if (Platform.isIOS) {
-      // Production ID: return 'ca-app-pub-4470739099972924/3546847705';
-      return 'ca-app-pub-3940256099942544/2934735716';
+      return 'ca-app-pub-5562747729301019/4643150374';
+      // Test unit ID: return 'ca-app-pub-3940256099942544/2934735716';
     } else {
       throw UnsupportedError('Unsupported platform');
     }
@@ -16,11 +16,11 @@ class AdHelper {
 
   static String get articleInterstitialAdUnitId {
     if (Platform.isAndroid) {
-      // Production ID: return 'ca-app-pub-4470739099972924/3193170043';
-      return 'ca-app-pub-3940256099942544/1033173712';
+      return 'ca-app-pub-5562747729301019/2856066616';
+      // Test unit ID: return 'ca-app-pub-3940256099942544/1033173712';
     } else if (Platform.isIOS) {
-      // Production ID: return 'ca-app-pub-4470739099972924/1582141296';
-      return 'ca-app-pub-3940256099942544/4411468910';
+      return 'ca-app-pub-5562747729301019/9731825521';
+      // Test unit ID: return 'ca-app-pub-3940256099942544/4411468910';
     } else {
       throw UnsupportedError('Unsupported platform');
     }
